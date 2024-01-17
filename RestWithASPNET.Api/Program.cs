6453c1b8973e332078e0361da6cce5d70fa3b366
Allan Victor <97359671+AllanVictorDeveloper@users.noreply.github.com>
2024-01-16 22:52:10 -0300

    Update Program.cs

--- RestWithASPNET.Api/Program.cs
+++ RestWithASPNET.Api/Program.cs
@@ -89,7 +89,7 @@ builder.Services.AddSwaggerGen(c =>
     {
         Title = "REST API's from 0 to Azure with ASP.NET Core 8 and Docker.",
         Version = "v1",
-        Description = "API RESTful developed in course REST API's from 0 to Azure with ASP.NET Core 8 and Docker.",
+        Description = "API RESTful developed in course REST API's from 0 to Azure with ASP.NET Core 8 and Docker - Foo bar.",
         Contact = new OpenApiContact
         {
             Name = "Allan Victor",
